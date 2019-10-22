a8c96d68672fb6180a8537b97b1e198ffa4f43de
Robert Woolley <rob@robertwoolley.co.uk>
2019-10-22 16:35:55 +0100

    Created Journey class and refactored methods and tests into it

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,7 +8,6 @@ class Oystercard
   def initialize
     @balance = 0
     @in_journey = false
-    @journey_history = {}
   end
 
   def top_up(credit)
@@ -18,33 +17,10 @@ class Oystercard
     @balance = new_balance
   end
 
-  def in_journey?
-    !!@origin_station
-  end
-
-
-  def touch_in(origin_station)
-    fail "Access denied. Card balance below min." unless @balance >= MIN_BALANCE
-    @origin_station = origin_station
-  end
-
-  def touch_out(dest_station)
-    deduct(1)
-    @dest_station = dest_station
-    save_journey_history
-    @origin_station = nil
-  end
-
-
-private
+# private
 
   def deduct(debit)
     @balance -= debit
   end
-
- def save_journey_history
-   @journey_history.store(@origin_station, @dest_station)
- end
-
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,9 @@
 require 'oystercard'
+require 'journey'
 
 describe Oystercard do
   let(:card) {Oystercard.new}
+  let(:journey) {Journey.new}
   let(:station) { double :station }
   let(:station2) { double :station2 }
 
@@ -20,11 +22,11 @@ describe Oystercard do
     end
 
     it "gets blocked by the gateline if the balance is below minimum" do
-      expect{ card.touch_in(station)}.to raise_error("Access denied. Card balance below min.")
+      expect{ journey.touch_in(card, station)}.to raise_error("Access denied. Card balance below min.")
     end
 
     it "reduces the card balance by 1.00 when card touches out" do
-      expect{ card.touch_out(station2)}.to change{card.balance}.by(-1)
+      expect{ journey.touch_out(card, station2)}.to change{card.balance}.by(-1)
     end
 
 
@@ -32,43 +34,43 @@ describe Oystercard do
 
   context "is the card in use?" do
     it "the card is in use" do
-      expect(card.in_journey?).to eq false
+      expect(journey.in_journey?).to eq false
     end
 
     it "allows you to touch in to start a journey" do
       card.top_up(Oystercard::MIN_BALANCE)
-      card.touch_in(station)
-      expect(card.in_journey?).to eq true
+      journey.touch_in(card, station)
+      expect(journey.in_journey?).to eq true
     end
 
     it "remembers the station it touched out at" do
-      card.touch_out(station2)
-      expect(card.dest_station).to eq(station2)
+      journey.touch_out(card, station2)
+      expect(journey.dest_station).to eq(station2)
     end
   end
 
   context "journey details" do
     it "has an empty journey history when card is spawned" do
-      expect(card.journey_history).to be_empty
+      expect(journey.journey_history).to be_empty
     end
 
    before(:each) do
      card.top_up(Oystercard::MIN_BALANCE)
-     card.touch_in(station)
+     journey.touch_in(card, station)
    end
 
     it "remembers the station it touched in at" do
-      expect(card.origin_station).to eq(station)
+      expect(journey.origin_station).to eq(station)
     end
 
     it "sets the origin station nil when touched out" do
-      card.touch_out(station2)
-      expect(card.origin_station).to eq(nil)
+      journey.touch_out(card, station2)
+      expect(journey.origin_station).to eq(nil)
     end
 
     it "has the origin and destination stations of one trip recorded" do
-      card.touch_out(station2)
-      expect(card.journey_history).to eq({station => station2})
+      journey.touch_out(card, station2)
+      expect(journey.journey_history).to eq({station => station2})
     end
 
   end
